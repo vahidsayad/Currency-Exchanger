8a8ba07ebf4f6b4f2f1c68a8d794d4123686f352
Vahid Sayad <vahidsayad@gmail.com>
2022-04-17 10:10:32 +0430

    Initial Commit

---
+++ Currency Exchanger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Currency Exchanger
+//
+//  Created by Vahid Sayad on 17/4/2022 .
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Currency Exchanger/Currency_ExchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ExchangerApp.swift
+//  Currency Exchanger
+//
+//  Created by Vahid Sayad on 17/4/2022 .
+//
+
+import SwiftUI
+
+@main
+struct Currency_ExchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
